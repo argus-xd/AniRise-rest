0b39b03b98c1aef1c6fe5979b111599afa9fce2c
CodeSprut <Alex-p.s@yandex.ru>
2020-11-04 04:17:01 +0500

    sort anime by rating implemented

--- src/controllers/anime.controller.js
+++ src/controllers/anime.controller.js
@@ -1,21 +1,56 @@
 const kodikClient = require("../clients/kodik");
 const animeDb = require("../services/anime-db");
+const get = require("lodash.get");
 
-const animeList = () => {
-  return kodikClient.getAnimeList();
+const pageLimit = limit => {
+  limit = limit || 100;
+
+  if (limit < 1) return 1;
+  if (limit > 100) return 100;
+
+  return limit;
+};
+
+const availableSorts = direction => ({
+  date: (a, b) => false,
+  rating: (a, b) => {
+    const aRating = get(a, "material_data.shikimori_rating", 0);
+    const bRating = get(b, "material_data.shikimori_rating", 0);
+
+    if (direction === "asc") {
+      return aRating - bRating;
+    } else {
+      return bRating - aRating;
+    }
+  }
+});
+
+const animeList = ({ query }) => {
+  const sortDirection = query["sort-direction"] || "desc";
+  const selectedSort =
+    availableSorts(sortDirection)[query["sort-field"]] ||
+    availableSorts(sortDirection).date;
+  const limit = pageLimit(query["limit"]);
+
+  console.log(sortDirection, limit);
+
+  return animeDb
+    .animeList()
+    .sort(selectedSort)
+    .slice(0, limit)
+    .map(listFormatMapper);
 };
 
 const animeSearch = ({ query }) => {
-  return animeDb.animeSearch(query["title"] || "").map(anime => ({
-    id: anime.id,
-    title: anime.title,
-    title_orig: anime.title_orig,
-    shikimori_id: anime.shikimori_id,
-    material_data: {
-      poster_url: anime.material_data.poster_url
-    }
-  }));
+  return animeDb.animeSearch(query["title"] || "").map(listFormatMapper);
 };
+
+const listFormatMapper = anime => ({
+  title: anime.title,
+  shikimoriId: anime.shikimori_id,
+  rating: get(anime, "material_data.shikimori_rating", 0),
+  poster: get(anime, "material_data.poster_url", "")
+});
 
 module.exports = {
   animeList,
