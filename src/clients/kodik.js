e357f9e957919afe3a4b5e179bf8adde004fb85b
CodeSprut <Alex-p.s@yandex.ru>
2020-11-06 18:31:30 +0500

    new kodik api method implemented

--- src/clients/kodik.js
+++ src/clients/kodik.js
@@ -3,23 +3,31 @@ const client = require("axios").create({
   baseURL: url
 });
 
-const getAnimeList = (limit = 100) => {
+const simpleGetRequest = (endpoint, params = {}) => {
   return client
-    .get("/list", {
-      params: {
-        token: authToken,
-        types: "anime-serial,anime",
-        with_episodes: "true",
-        with_material_data: "true",
-        limit
-      }
-    })
-    .then(({ data }) => {
-      return data.results || [];
-    })
+    .get(endpoint, { params })
+    .then(({ data }) => data.results)
     .catch(() => []);
 };
 
+const getAnimeList = (limit = 100) => {
+  return simpleGetRequest("/list", {
+    token: authToken,
+    types: "anime-serial,anime",
+    with_episodes: "true",
+    with_material_data: "true",
+    limit
+  });
+};
+
+const getAnimeByShikimoriId = id => {
+  return simpleGetRequest("/search", {
+    token: authToken,
+    shikimori_id: id
+  });
+};
+
 module.exports = {
-  getAnimeList
+  getAnimeList,
+  getAnimeByShikimoriId
 };
