77de289702fd348295a0a9af4b0e24236d13a7b3
CodeSprut <Alex-p.s@yandex.ru>
2020-11-08 18:45:54 +0500

    anime cache container now saves full list with duplicates

--- src/services/anime-db.js
+++ src/services/anime-db.js
@@ -3,11 +3,6 @@ const { animeDb } = require("../config");
 const miniSearch = require("minisearch");
 const animeSorter = require("../utils/anime-sorter");
 
-const timeElapsedLabels = {
-  search: "Search time",
-  cacheUpdate: "Cache update elapsed time"
-};
-
 const searchEngine = new miniSearch({
   fields: ["title", "title_orig"],
   storeFields: ["shikimori_id", "title"]
@@ -19,8 +14,6 @@ const searchResultsLimit = 30;
 const animeList = () => cachedAnime;
 
 const animeSearch = searchTerm => {
-  console.time(timeElapsedLabels.search);
-
   const result = [];
   const searchResults = searchEngine.search(searchTerm, { fuzzy: 0.2 });
 
@@ -37,38 +30,35 @@ const animeSearch = searchTerm => {
     }
   }
 
-  console.timeEnd(timeElapsedLabels.search);
-
   return result;
 };
 
 const updateCache = async () => {
-  console.time(timeElapsedLabels.cacheUpdate);
+  const timeElapsedLabel = "Cache update elapsed time";
+  console.time(timeElapsedLabel);
 
-  const downloadedAnime = [];
+  let downloadedAnime = [];
 
   for (const dumpName of animeDb.dumpsList) {
-    const dumpAnime = (await downloadDump(dumpName)).sort(
-      animeSorter.select("date", "desc")
-    );
-
-    dumpAnime.forEach((anime, index) => {
-      const foundIndex = dumpAnime.findIndex(
-        found => found.shikimori_id === anime.shikimori_id
-      );
-
-      if (index === foundIndex) {
-        downloadedAnime.push(anime);
-      }
-    });
+    const dumpAnime = await downloadDump(dumpName);
+    downloadedAnime = [...downloadedAnime, ...dumpAnime];
   }
 
+  downloadedAnime = downloadedAnime.sort(animeSorter.select("date", "desc"));
+  const uniqueAnime = downloadedAnime.filter((anime, index) => {
+    const foundIndex = downloadedAnime.findIndex(
+      found => found.shikimori_id === anime.shikimori_id
+    );
+
+    return index === foundIndex;
+  });
+
   searchEngine.removeAll();
-  searchEngine.addAll(downloadedAnime);
+  searchEngine.addAll(uniqueAnime);
   cachedAnime = downloadedAnime;
 
   console.log("Anime in cache: " + cachedAnime.length);
-  console.timeEnd(timeElapsedLabels.cacheUpdate);
+  console.timeEnd(timeElapsedLabel);
 };
 
 const downloadDump = dumpName => {
