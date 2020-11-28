29e5270f5123c38e52c9da6ba17ab5b19a2d5ec0
CodeSprut <Alex-p.s@yandex.ru>
2020-11-29 02:38:34 +0500

    fix unexisted fields accessing

--- src/clients/shikimori.js
+++ src/clients/shikimori.js
@@ -29,7 +29,7 @@ const infoById = async id => {
     url: animeInfo.url,
     poster: animeInfo.image?.original ?? "",
     description: animeInfo.description,
-    genres: animeInfo.genres.map(genre => genre.name),
+    genres: (animeInfo?.genres ?? []).map(genre => genre.name),
     franchise: (franchise?.nodes ?? []).map(({ id, date }) => ({
       id,
       date
