d54ed439c65be2a02eb60c385bda5d5de3e16554
CodeSprut <Alex-p.s@yandex.ru>
2020-11-29 02:09:16 +0500

    move method from service to client

--- src/clients/shikimori.js
+++ src/clients/shikimori.js
@@ -19,4 +19,22 @@ const franchiseById = id => {
   return simpleGetRequest(`/api/animes/${id}/franchise`);
 };
 
-module.exports = { animeById, franchiseById };
+const infoById = async id => {
+  const animeInfo = await animeById(id);
+  const franchise = await franchiseById(id);
+
+  return {
+    title: animeInfo.russian,
+    titleEng: animeInfo.name,
+    url: animeInfo.url,
+    poster: animeInfo.image?.original ?? "",
+    description: animeInfo.description,
+    genres: animeInfo.genres.map(genre => genre.name),
+    franchise: (franchise?.nodes ?? []).map(({ id, date }) => ({
+      id,
+      date
+    }))
+  };
+};
+
+module.exports = { animeById, franchiseById, infoById };
