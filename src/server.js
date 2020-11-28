65ba1fec1323d1895b569b5b4202fc11a9c95a8d
CodeSprut <Alex-p.s@yandex.ru>
2020-11-29 00:17:22 +0500

    morgan and body-parser dependencies removed

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
 const cors = require("cors");
 const router = require("./router");
 const cacheContainer = require("./cache-container");
@@ -12,18 +10,7 @@ const { restPort } = require("./config");
 
   const app = express();
 
-  app.use(morgan("tiny"));
   app.use(cors());
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
   app.use(router);
 
   app.listen(restPort, () => {
